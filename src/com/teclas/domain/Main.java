d6eeee693a0c311f5a916db3ade4eda0abf93421
stvteclas <stvpablo@gmail.com>
2022-10-15 10:24:20 -0300

    firs commit

---
+++ src/com/teclas/domain/Main.java
@@ -0,0 +1,39 @@
+package com.teclas.domain;
+
+/**
+ * Documentando las impresiones de todos los tipos de variables
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+    //Enteros
+        byte var1 = 6;
+        System.out.println("var1 = " + var1);
+        short var2 = 15;
+        System.out.println("var2 = " + var2);
+        int var3 = 45;
+        System.out.println("var3 = " + var3);
+        long var4 = 1234558983;
+        System.out.println("var4 = " + var4);
+
+        //decimales
+        float var5 = 7.3f;
+        System.out.println("var5 = " + var5);
+        double var6 = 13.33;
+        System.out.println("var6 = " + var6);
+        //booleano
+        boolean var7 = false;
+        System.out.println("var7 = " + var7);
+
+        boolean var8 = true;
+        System.out.println("var8 = " + var8);
+        //texto
+        char var9 = 'c';
+        System.out.println("var9 = " + var9);
+        String var10 = "feliz dia de las madres ";
+        System.out.println("var10 = " + var10);
+
+
+    }
+}
